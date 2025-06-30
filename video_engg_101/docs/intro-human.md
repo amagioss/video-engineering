b8b5a4aadc3b938d06064164bc4477bff9b0a9bd
vinod-amagi <vinodpr@amagi.com>
2025-06-30 10:42:01 +0000

    docs: Update introduction

--- video_engg_101/docs/intro-human.md
+++ video_engg_101/docs/intro-human.md
@@ -1,6 +1,6 @@
 ---
 sidebar_position: 2
-title: Introduction by Human
+title: Introduction
 ---
 
 # Introduction
--- video_engg_101/sidebars.ts
+++ video_engg_101/sidebars.ts
@@ -3,8 +3,8 @@ import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
 
 const sidebars: SidebarsConfig = {
   tutorialSidebar: [
-    'intro-gpt',
     'intro-human',
+    'intro-gpt',
     {
       type: 'category',
       label: 'Multimedia Basics',
