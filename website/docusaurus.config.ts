072975794025fe8d8e28c6d156225e4894a072f7
Shibly Meeran <shibme@shib.me>
2025-07-22 16:54:55 +0530

    fixing base url

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -11,10 +11,10 @@ const config: Config = {
   tagline: 'Learn video engineering',
   favicon: 'img/favicon.ico',
 
-  url: 'https://video.amagiengg.io',
+  url: 'https://dev.amagi.com/video-engineering',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/video-engineering/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
